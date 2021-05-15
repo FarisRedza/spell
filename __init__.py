532299a57a670ffc46d77c723703cfa3afd9c091
Faris Redza <faris.redza1999@gmail.com>
2021-05-15 08:04:14 +0100

    Changed version number to 0.4.0, changed language to en_GB

--- __init__.py
+++ __init__.py
@@ -3,17 +3,15 @@
 from os import path
 
 import hunspell
-from albertv0 import *
+from albert import *
 
-__iid__ = "PythonInterface/v0.1"
-__prettyname__ = "Spell"
-__version__ = "0.1"
-__trigger__ = "spell "
-__author__ = "Bharat Kalluri"
-__dependencies__ = ['hunspell']
+__title__ = "Spell"
+__version__ = "0.4.0"
+__triggers__ = "spell "
+__authors__ = ["Bharat Kalluri", "Faris Redza"]
+__py_deps__ = ["hunspell"]
 
-icon_path = "{}/icons/{}.png".format(path.dirname(__file__), "correction")
-
+iconPath = "{}/icons/{}.png".format(path.dirname(__file__), "correction")
 
 def handleQuery(query):
     if query.isTriggered:
@@ -24,23 +22,23 @@ def handleQuery(query):
             return parse_input(query.string)
         else:
             return Item(
-                id=__prettyname__,
-                icon=icon_path,
-                text=__prettyname__,
+                id=__title__,
+                icon=iconPath,
+                text=__title__,
                 subtext="Type in a spelling to see the correct spelling",
                 completion=query.rawString,
             )
 
 
 def parse_input(word):
-    spellchecker = hunspell.HunSpell('/usr/share/hunspell/en_US.dic', '/usr/share/hunspell/en_US.aff')
+    spellchecker = hunspell.HunSpell('/usr/share/hunspell/en_GB.dic', '/usr/share/hunspell/en_GB.aff')
     response = spellchecker.suggest(word)
     results = []
     if len(response) > 0:
         for replacements in response:
             results.append(Item(
-                id=__prettyname__,
-                icon=icon_path,
+                id=__title__,
+                icon=iconPath,
                 text=replacements,
                 subtext='',
                 actions=[ClipAction("Copy to clipboard", str(replacements))]
